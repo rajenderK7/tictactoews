35a430b691873be5c3a35a05059436318d50b5df
Rajender <rajenderkatkuri123@gmail.com>
2024-06-16 00:07:12 +0530

    feat: add graceful shutdown to server

--- server/cmd/tictactoews/main.go
+++ server/cmd/tictactoews/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
@@ -32,5 +36,21 @@ func main() {
 		return nil
 	})
 
-	e.Start("localhost:4000")
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	// Start server
+	go func() {
+		if err := e.Start(":4000"); err != nil && err == http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
